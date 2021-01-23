e77034cef10c87f86b06de444c2a18dea0bf8c06
Susana <susananzth@gmail.com>
2021-01-23 12:48:05 -0500

    Variables y constantes

--- js/iniciando.js
+++ js/iniciando.js
@@ -16,7 +16,26 @@ las buenas practicas a la hora de escribir código.*/
 /* Con esta línea de comando al príncipio del archivo JS, 
 se asegura el strict Mode del código */
 
-/* Variables */
+
+/* ==== Variables ==== */
+
+// Variables Globales, estan declaradas fueda de las funciones
 var nombre = "Susana";
+// Imprimo el contenido de la variable en la consola
+console.log(nombre);
 
+/* Funcion saludo, las funciones son blques de código
+que puedp reutilizar más adelante en este archivo o en otros archivos */
+function saludo(){
+    // Variable local
+    let edad = 28;
+    
+    // Imprimo el contenido de la variable en la consola
+    console.log(edad);
+}
+// Llamo la función
+saludo();
 
+/* ==== Constantes ==== */
+
+const pi = 3.1416;
