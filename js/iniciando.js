f9b664403567afef495037dd6e994ee86c44f414
Susana <susananzth@gmail.com>
2021-01-23 12:27:51 -0500

    Iniciando con Js

---
+++ js/iniciando.js
@@ -0,0 +1,22 @@
+// Esto es un comentario de una línea
+
+/* Esto es 
+un comentario 
+de multiples líneas o bloque */
+
+/* Mensaje simple del navegador */
+alert("Holis!");
+
+/* Mensaje a la consola del navegador */
+console.log("Hola consola");
+
+/* strict Mode para asegurar
+las buenas practicas a la hora de escribir código.*/
+"use strict" 
+/* Con esta línea de comando al príncipio del archivo JS, 
+se asegura el strict Mode del código */
+
+/* Variables */
+var nombre = "Susana";
+
+
