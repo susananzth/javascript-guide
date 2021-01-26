9143556f4a6afe6b03bb8be623dc1622c676f86d
susananzth <susananzth@gmail.com>
2021-01-26 15:06:31 -0500

    Buenas practicas de los estilos del código

---
+++ js/B-P-estilos.js
@@ -0,0 +1,39 @@
+// Comentarios generales
+/* - Uso correcto del , y ;
+   - Identar el código
+   - Las comillas '' y "" se pueden usar, fijarse que sea la mismas que inicia y termina.
+   - Uso de comentarios para documentar, utilizar la referencia de JSdocs.
+   - Utilizar JSlint.com para validar el código. Sólo para consejos, no para guía definitiva.
+
+*/
+
+// **********************
+// Formato de nombre de valiables
+
+// Estilo comel case
+var nombreCompleto = "Sergio Brito";
+
+// Estilo Snake case
+var nombre_completo = "Sergio Brito";
+
+// Para funciones de igual forma se debe seguir un patrón standard
+function saludarPersona(){
+
+}
+
+// **********************
+// Formato de nombre de contantes
+
+// Declarar las contantes en mayúsculas
+var LIMITE = 30;
+
+// user el formato snake case para variables con dos palabras, no el camel case
+var LIMITE_TOTAL = 30;
+
+// **********************
+// Formato de abreviatura de variables
+var arrAnimales = ["perro", "gato","pez"];
+
+var btnSaludar = document.getElementById("btnSaludar");
+
+var frmRegistro =  document.getElementById("frmRegistro");
